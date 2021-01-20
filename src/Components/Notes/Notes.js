f7ce3e214cf97e017e720d548bf0c054c7671935
Shivashankar-gitH <shiva1894300@gmail.com>
2021-01-20 12:25:01 +0530

    popup

--- src/Components/Notes/Notes.js
+++ src/Components/Notes/Notes.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Notes.css";
 import { NameContext } from "../State/NameContext";
+import { IoMdSettings } from "react-icons/io";
 
 const Notes = () => {
 	const [name] = React.useContext(NameContext);
+	const [popup, setPopup] = useState(true);
+	const date = new Date().getDate();
+	const month = new Date().getMonth();
+	const year = new Date().getFullYear();
 
 	return (
-		<div>
-			<h1>welcome {name}</h1>
+		<div className="Notes">
+			<div
+				className={popup ? "settings--popupActive" : "settings--popup"}
+			></div>
+			<div className="Notes--details">
+				<div className="Notes--namenddata">
+					<h1 className="Notes--name">welcome {name}</h1>
+					<span className="Notes--date">{`${date} / ${
+						month + 1
+					} / ${year}`}</span>
+				</div>
+				<div className="Notes--settingsBtn" onClick={() => setPopup(!popup)}>
+					<div className="Notes--settingsIcon">
+						<IoMdSettings className="Notes--settingsIconIn" />
+					</div>
+					Settings
+				</div>
+			</div>
 		</div>
 	);
 };
